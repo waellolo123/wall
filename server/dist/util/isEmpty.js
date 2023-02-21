a8c5082bb82b53b81b786b18d25dfad927ba657a
mansouri youssef <mansouriyoussef1991@gmail.com>
2023-02-21 10:48:44 +0100

    First commit

--- server/dist/util/isEmpty.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const isEmpty = (value) => value === null ||
-    value === undefined ||
-    (typeof value === "object" && Object.keys(value).length === 0) ||
-    (typeof value === "string" && value.trim().length === 0);
-exports.default = isEmpty;
