19fecdca6168635636db874d6727c35362d54ae7
mansouri youssef <mansouriyoussef1991@gmail.com>
2023-02-21 01:12:37 +0100

    First commit

--- server/app.ts
+++ server/app.ts
@@ -35,10 +35,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  "/static",
-  express.static(path.join(__dirname.replace("/dist", ""), "public/images"))
-);
+app.use("/static", express.static("public/images"));
 
 /* Connect to DB */
 const MONGO_URI = process.env.MONGO_URI!;
